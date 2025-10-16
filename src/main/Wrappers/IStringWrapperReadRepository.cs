abbe6be6ba32e3174608f1d703b3391cb2367e4c
elmerbool <35117496+elmerbool@users.noreply.github.com>
2025-10-17 08:28:34 +1100

    Refactored StringWrapperRepository into Read and Write repos.

--- src/main/Wrappers/IStringWrapperReadRepository.cs
+++ src/main/Wrappers/IStringWrapperReadRepository.cs
@@ -9,7 +9,7 @@ namespace ei8.Cortex.Coding.Persistence.Wrappers
     /// <summary>
     /// Provides functionality for retrieving StringWrappers.
     /// </summary>
-    public interface IStringWrapperRepository
+    public interface IStringWrapperReadRepository
     {
         /// <summary>
         /// Gets StringWrappers using the specified IDs.
@@ -21,13 +21,5 @@ namespace ei8.Cortex.Coding.Persistence.Wrappers
             IEnumerable<Guid> ids,
             CancellationToken token = default
         );
-
-        /// <summary>
-        /// Saves the specified StringWrapper value.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task Save(StringWrapper value, CancellationToken token = default);
     }
 }
---
+++ src/main/Wrappers/IStringWrapperWriteRepository.cs
@@ -0,0 +1,23 @@
+﻿using ei8.Cortex.Coding.Wrappers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Coding.Persistence.Wrappers
+{
+    /// <summary>
+    /// Provides functionality for writing StringWrappers.
+    /// </summary>
+    public interface IStringWrapperWriteRepository
+    {
+        /// <summary>
+        /// Saves the specified StringWrapper value.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task Save(
+            StringWrapper value, 
+            CancellationToken token = default
+        );
+    }
+}
